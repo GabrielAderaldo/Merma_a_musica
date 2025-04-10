f280650f015a9bcf262b4bc59b6d511f3f13dd3b
Gabriel Aderaldo <gabrieladeraldo@Mac-mini-de-Gabriel.local>
2025-04-09 22:12:41 -0300

    [00] - nothing here

--- index.ts
+++
@@ -1,8 +0,0 @@
-const teste = Bun.serve({
-    port: 3000,
-    fetch(request) {
-        return new Response("Hello World");
-    }
-})
-
-console.log("Server running on http://localhost:", teste.port);
